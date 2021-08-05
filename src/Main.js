465c79cae2d5db3b08cdd97b5e4452506999784d
Justin W Soto <soto@pdx.edu>
2021-08-04 18:25:34 -0700

    good enough

--- src/Main.js
+++ src/Main.js
@@ -7,22 +7,30 @@ import pizza from './assets/choco-pizza.png';
 import divider from './assets/Capture.PNG';
 import vanessa from './assets/van-pic.png';
 
-class Body extends Component {
+class Main extends Component {
    
     render() { 
         return ( 
+
             <div>
                    <h1>Chocolate Pizza</h1>
+
             <h2>POSTED ON 15 DEC 2013 / DESSERTS</h2>
             
             <section className="print-section">
-            <h2 className="print-txt">PRINT</h2>
-            <img className="print-img"src={print} alt="print"/>
+
+                <h2 className="print-txt">PRINT</h2>
+
+                    <img className="print-img"src={print} alt="print"/>
+
             </section>
             
             <section className="pizza-img">
-            <img className="pizza" src={pizza} alt="pizza"/>
+
+                <img className="pizza" src={pizza} alt="pizza"/>
+
             </section>
+
             <section className="main-section">
 
 <section className="p-section">
@@ -45,41 +53,52 @@ class Body extends Component {
 </section>
 
 <section className="ingredients">
-<span>1 1/2 cups milk </span>
-<span>1/2 cup mascarpone</span>
-<span>1/2 tsp pink salt</span>
-<span>1/2 cup brown sugar</span>
-<span>1 lb black mission figs</span>
-<span>2-4 Tbsp water</span>
-<span>1 1/2 cups heavy cream</span>
-<span id="strike">1/3 cup granulated sugar</span>
-<span>1 lemon, juiced</span>
-<span id="strike2">2 egg yolks</span>
-<span>2 Tbsp butter</span>
-<span>1 cup honey roasted pecans, roughly chopped</span>
+
+    <span>1 1/2 cups milk </span>
+    <span>1/2 cup mascarpone</span>
+    <span>1/2 tsp pink salt</span>
+    <span>1/2 cup brown sugar</span>
+    <span>1 lb black mission figs</span>
+    <span>2-4 Tbsp water</span>
+    <span>1 1/2 cups heavy cream</span>
+    <span id="strike">1/3 cup granulated sugar</span>
+    <span>1 lemon, juiced</span>
+    <span id="strike2">2 egg yolks</span>
+    <span>2 Tbsp butter</span>
+    <span>1 cup honey roasted pecans, roughly chopped</span>
 
 </section>
 
 </section>
+
 <section className="divider2">
-<img src={divider} alt='divider'/>
+
+    <img src={divider} alt='divider'/>
+
 </section>
 
 
 <section className="vanessa">
+
 <section className="vanessa-img">
+
     <img src={vanessa} alt='vanessa'/>
+
 </section>
 
 <section className="vanessa-name">
-<span className="name">Vanessa Stevenson</span>
-<br></br>
-<span className="bio">Food enthusiast, photography fan. Add a pinch of raw foodism and that's pretty much who I am.</span>
+
+    <span className="name">Vanessa Stevenson</span>
+
+    <span className="bio">Food enthusiast, photography fan. Add a pinch of raw foodism and that's pretty much who I am.</span>
+
 </section>
 
 
-<section className="vanessa-btn">
+<section>
+
 <button className="btn">SHARE RECIPE</button>
+
 </section>
 
 </section>
@@ -88,4 +107,4 @@ class Body extends Component {
     }
 }
  
-export default Body;
+export default Main;
